9881c59083b233116305161a918521b9bf407231
Huy <duchuykun@gmail.com>
2017-07-26 23:14:19 +0700

    Update main

--- DummiesData.tableplusplugin/main.js
+++ DummiesData.tableplusplugin/main.js
@@ -5,42 +5,45 @@ var randomSentence = require('random-sentence');
 // Get table in opening tab
 var table = Workspace.currentTable();
 
+function main(context) {
+
 // Fetch table info
-table.info(function(info) {
+    table.info(function(info) {
 
-    // Get table columns listing
-    var columns = info['columns'];
-    
-    for (var i = 0; i < 10; i++) {
-        // Add empty row
-        var row = table.addEmptyRow(columns); 
+        // Get table columns listing
+        var columns = info['columns'];
+        
+        for (var i = 0; i < 10; i++) {
+            // Add empty row
+            var row = table.addEmptyRow(columns); 
 
-        var keys = Object.keys(columns);
+            var keys = Object.keys(columns);
 
-        // Random value
-        keys.forEach(function(key) {
-            var dataType = columns[key]['typeString'];
-            if (dataType == 'varchar' || dataType == 'text') {
-                var sentence = randomSentence();
-                row.update(key, sentence);
-                row.setDefault(key, 0);            
-            }
-            if (dataType == 'int4' && key != 'id') {
-                var number = Math.floor(Math.random() * 1000000);
-                row.update(key, number);
-                row.setDefault(key, 0);               
-            }
-            if (dataType == 'timestamp') {
-                var t = new Date();
-                var formatted = t.toISOString();
-                row.update(key, formatted);
-                row.setDefault(key, 0);               
-            }
-        });
+            // Random value
+            keys.forEach(function(key) {
+                var dataType = columns[key]['typeString'];
+                if (dataType == 'varchar' || dataType == 'text') {
+                    var sentence = randomSentence();
+                    row.update(key, sentence);
+                    row.setDefault(key, 0);            
+                }
+                if (dataType == 'int4' && key != 'id') {
+                    var number = Math.floor(Math.random() * 1000000);
+                    row.update(key, number);
+                    row.setDefault(key, 0);               
+                }
+                if (dataType == 'timestamp') {
+                    var t = new Date();
+                    var formatted = t.toISOString();
+                    row.update(key, formatted);
+                    row.setDefault(key, 0);               
+                }
+            });
 
-        table.addToInsert(row);
-    }
+            table.addToInsert(row);
+        }
 
-    // Reload workspace view
-    Workspace.reload();  
-});
+        // Reload workspace view
+        Workspace.reload();  
+    });
+};
