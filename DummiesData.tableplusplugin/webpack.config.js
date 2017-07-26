fc108a129e083d14602145acc978497a0290832e
Huy <duchuykun@gmail.com>
2017-07-27 01:20:32 +0700

    Update config

--- DummiesData.tableplusplugin/webpack.config.js
+++ DummiesData.tableplusplugin/webpack.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
   entry: './main.js',
   output: {
     filename: 'bundle.js'
-  }
-}
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+};
+
+module.exports = config;
